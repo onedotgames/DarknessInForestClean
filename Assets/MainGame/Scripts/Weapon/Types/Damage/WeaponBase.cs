0625554b589bf3f5f800d5bd70df3242897f628f
berkayOneDot <102809142+berkayOneDot@users.noreply.github.com>
2022-12-16 15:28:31 +0300

    16.12.22
    +healthpotion barrel system a eklendi.

--- Assets/MainGame/Scripts/Weapon/Types/Damage/WeaponBase.cs
+++ Assets/MainGame/Scripts/Weapon/Types/Damage/WeaponBase.cs
@@ -545,7 +545,7 @@ public class WeaponBase :CustomBehaviour
             BarrelPooler.ReturnObjectToPool(collision.gameObject);
             GameManager.BarrelSystem.barrelCount--;
             // coin magnet ya da bomb spawn olacak.
-            var k = Random.Range(0, 10);
+            var k = Random.Range(0, 13);
             if(k < 3)//bomba?
             {
                 var bombPool = GameManager.PoolingManager.CollectablePoolerList[(int)CollectablePoolerType.BombPooler];
@@ -558,11 +558,17 @@ public class WeaponBase :CustomBehaviour
                 var magnet = magnetPool.GetObjectFromPool();
                 magnet.transform.position = barrelPos;
             }
-            else if(k >= 6) //co?in
+            else if(k >= 6 && k < 9) //co?in
             {
                 var coinPool = GameManager.PoolingManager.CoinPoolerList[(int)CoinType.Small];
                 var coin = coinPool.GetObjectFromPool();
                 coin.transform.position = barrelPos;
+            }
+            else if (k >= 9 && k < 13)//healthPot
+            {
+                var healthPotPool = GameManager.PoolingManager.CollectablePoolerList[(int)CollectablePoolerType.HealthPotPooler];
+                var healthPot = healthPotPool.GetObjectFromPool();
+                healthPot.transform.position = barrelPos;
             }
         }
     }
