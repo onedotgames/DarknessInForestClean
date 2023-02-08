13e1f844a65caa6e0b88e8dbfaa9a43b08f98926
erselonedot <102801699+erselonedot@users.noreply.github.com>
2023-02-08 12:39:22 +0300

    08.02.23 AI fixes

--- Assets/MainGame/Scripts/AI/AllyAI.cs
+++ Assets/MainGame/Scripts/AI/AllyAI.cs
@@ -5,5 +5,14 @@ using UnityEngine.AI;
 
 public class AllyAI : CustomBehaviour
 {
-   
+    private Transform playerTransform;
+
+    public override void Initialize(GameManager gameManager)
+    {
+        base.Initialize(gameManager);
+        playerTransform = GameManager.PlayerManager.CurrentPlayer.transform;
+    }
+
+    public enum ActionState { IDLE, WORKING};
+    private ActionState state = ActionState.IDLE;
 }
